40c7b3736a1642d04cf2d73b5ea30fdd836dceb2
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-12-01 21:45:48 -0600

    starting working on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { Box, Typography } from "@mui/material";
 
 const About = () => {
   return (
     <Layout>
-      <h1>About</h1>
+      <Box>
+        <Typography variant="h4">Welcome to my restaurant</Typography>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+          culpa quam in possimus ad velit dolores necessitatibus atque,
+          doloremque porro earum dicta ipsa incidunt labore quo! Incidunt cumque
+          facere optio.
+        </p>
+      </Box>
     </Layout>
   );
 };
