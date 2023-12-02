cb5e935b9c7d34f6d7431595a5610e512953b594
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-12-01 21:38:16 -0600

    done the home pages

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -120,7 +120,7 @@ const Header = () => {
             {drawer}
           </Drawer>
         </Box>
-        <Box sx={{p:1}}>
+        <Box>
           <Toolbar />
         </Box>
       </Box>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import { Link } from "react-router-dom";
+import Banner from "../images/banner.jpg";
+import "../styles/HomeStyle.css";
 
 const Home = () => {
   return (
     <Layout>
-      <div className="home">
+      <div className="home" style={{backgroundImage:`url(${Banner})`}}>
         <div className="headerContainer">
           <h1>T Jin China Diner</h1>
           <p>Food in China</p>
